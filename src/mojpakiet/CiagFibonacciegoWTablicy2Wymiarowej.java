b8900cbe4a5fab9d432476a0950dc497110f9902
Abaddon7063554 <109863155+Abaddon7063554@users.noreply.github.com>
2023-11-14 14:35:17 +0100

    Abaddon

--- src/mojpakiet/CiagFibonacciegoWTablicy2Wymiarowej.java
+++ src/mojpakiet/CiagFibonacciegoWTablicy2Wymiarowej.java
@@ -5,14 +5,48 @@ public class CiagFibonacciegoWTablicy2Wymiarowej{
     public static void main(String[] args) {
 
         int[] tab1 = new int[10];
-        int l1=1,l2=1,l3;
-        tab1[0]=l1;
-        tab1[1]=l2;
-        for(int  i = 2 ; i<10; i++){
-            tab1[i]=tab1[i-2]+tab1[i-1];
+        int l1 = 1, l2 = 1, l3;
+        tab1[0] = l1;
+        tab1[1] = l2;
+        tab[0][0] = l1;
+        tab[0][1] = l2;
+
+        for (int i = 2; i < 10; i++) {
+            tab1[i] = tab1[i - 2] + tab1[i - 1];
+            // for(int j = 2 ; j<10;j++){
+
+            // }
         }
-        for(int  i = 0 ; i<10; i++){
-            System.out.print(tab1[i]+" ");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(tab1[i] + " ");
+        }
+        System.out.println("\n \n");
+
+        for (int i = 2; i < 10; i++) {
+            tab[0][i] =tab[0][i - 2] + tab[0][i - 1];
+        }
+        tab[1][9]=tab[0][8]+tab[0][9];
+        tab[2][9]=tab[0][9]+tab[1][9];
+        for (int i = 2; i < 10; i++) {
+            tab[i][9] =tab[i-2][9] + tab[i-1][9];
+        }
+        tab[9][9]=tab[7][9]+tab[8][9];
+        tab[9][8]=tab[8][9]+tab[9][9];
+
+
+        for (int j = 9 ;j>-1;j--){
+
+           }
+
+
+
+
+        for (int j = 0; j < 10; j++){
+            for (int i = 0; i < 10; i++) {
+                System.out.print(tab[j][i] + " ");
+            }
+            System.out.println();
         }
 
     }
